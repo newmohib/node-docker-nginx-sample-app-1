f566e53ba532f705008e98c0e73ebacda29b7cb7
Mohibur Rahman <36856097+newmohib@users.noreply.github.com>
2025-02-19 17:03:17 +0600

    update script.groovy for jenkinsfile external script

--- script.groovy
+++ script.groovy
@@ -11,8 +11,9 @@ def testApp(){
 def buildImage(){
   echo 'building the docker image...'
   sh 'node -v && npm i && docker -v && docker images && docker ps -a'
-  // sh 'docker build -t newmohib/node-docker-nginx-sample-app:node-1.0.2 .'
+  sh 'docker build -t newmohib/node-docker-nginx-sample-app:node-1.0.2 .'
 }
+
 def pushToDockerHub(){
   echo 'Pushing the docker image to docker hub...'
   // withCredentials([usernamePassword(credentialsId:'docker-hub-personal-credential',passwordVariable:'PASS', usernameVariable:'USER')]){
@@ -24,5 +25,42 @@ def pushToDockerHub(){
 def deployApp(){
   echo 'deploying the application...'
 }
+
+// with check
+
+// check build iamge with git hash id into git
+def buildImageWithCheckExistes(){
+  echo 'building the docker image...'
+  sh 'node -v && npm i && docker -v && docker images && docker ps -a'
+
+  // Generate a unique tag using the latest Git commit hash
+  def imageTag = sh(script: "git rev-parse --short HEAD", returnStdout: true).trim()
+  env.IMAGE_TAG = imageTag
+
+  // Build the Docker image
+  sh "docker build -t newmohib/node-docker-nginx-sample-app:${imageTag} ."
+}
+
+// push image to docker hub with check exist
+
+def pushToDockerHubWithCheckExistes(){
+  echo 'Checking if the image exists on Docker Hub...'
+
+  def imageExists = sh(script: """
+    docker pull newmohib/node-docker-nginx-sample-app:\$IMAGE_TAG > /dev/null 2>&1 && echo 'exists' || echo 'not exists'
+  """, returnStdout: true).trim()
+
+  if (imageExists == 'exists') {
+    echo "Image already exists on Docker Hub. Skipping push."
+  } else {
+    echo "Pushing the new Docker image to Docker Hub..."
+    withCredentials([usernamePassword(credentialsId:'docker-hub-personal-credential', passwordVariable:'PASS', usernameVariable:'USER')]) {
+      sh 'echo $PASS | docker login -u $USER --password-stdin'
+      sh "docker push newmohib/node-docker-nginx-sample-app:\$IMAGE_TAG"
+    }
+  }
+}
+
+
 
 return this
