abc0a7a5d2aa16136aaff55160e910bdcc067cde
Mohibur Rahman <36856097+newmohib@users.noreply.github.com>
2025-02-19 16:09:19 +0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,9 +1,26 @@
-def buildApp(){
-  echo 'building the application'
+
+def installPackage(){
+  echo 'installing the application...'
+  sh 'node -v && npm i'
+}
+
+def buildImage(){
+  echo 'building the docker image...'
+  sh 'node -v && npm i && docker -v && docker images && docker ps -a'
+  // withCredentials([usernamePassword(credentialsId:'docker-hub-personal-credential',passwordVariable:'PASS', usernameVariable:'USER')]){
+  //   sh 'docker build -t newmohib/node-docker-nginx-sample-app:node-1.0.2 .'
+  //   sh 'echo $PASS | docker login -u $USER --password-stdin'
+  //   sh 'docker push newmohib/node-docker-nginx-sample-app:node-1.0.2'
+  // }
 }
 
 def deployApp(){
   echo 'deploying the application...'
 }
+
+def testApp(){
+  echo 'testing the application...'
+}
+
 
 return this
