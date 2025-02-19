335461ed286c7b8e6a70b3f44d76943580e4106c
Mohibur Rahman <36856097+newmohib@users.noreply.github.com>
2025-02-19 16:43:13 +0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,23 +4,25 @@ def installPackage(){
   sh 'node -v && npm i'
 }
 
+def testApp(){
+  echo 'testing the application...'
+}
+
 def buildImage(){
   echo 'building the docker image...'
   sh 'node -v && npm i && docker -v && docker images && docker ps -a'
-  // withCredentials([usernamePassword(credentialsId:'docker-hub-personal-credential',passwordVariable:'PASS', usernameVariable:'USER')]){
-  //   sh 'docker build -t newmohib/node-docker-nginx-sample-app:node-1.0.2 .'
-  //   sh 'echo $PASS | docker login -u $USER --password-stdin'
-  //   sh 'docker push newmohib/node-docker-nginx-sample-app:node-1.0.2'
-  // }
+  sh 'docker build -t newmohib/node-docker-nginx-sample-app:node-1.0.2 .'
+}
+def pushToDockerHub(){
+  echo 'Pushing the docker image to docker hub...'
+  withCredentials([usernamePassword(credentialsId:'docker-hub-personal-credential',passwordVariable:'PASS', usernameVariable:'USER')]){
+    sh 'echo $PASS | docker login -u $USER --password-stdin'
+    sh 'docker push newmohib/node-docker-nginx-sample-app:node-1.0.2'
+  }
 }
 
 def deployApp(){
   echo 'deploying the application...'
 }
-
-def testApp(){
-  echo 'testing the application...'
-}
-
 
 return this
