af60078ff9257f3a98b62b2f370c52b5fe36d85d
newmohib <@@mohib100@@>
2025-02-20 15:59:36 +0600

    fix groovy issue

--- src/com.example/Docker.groovy
+++ src/com.example/Docker.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-package com.example
+// package com.example
 
 class Docker implements Serializable {
     def script
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
 
-package com.example.Docker
+// package com.example.Docker
 
 def call(String imageName){
-    return new Docker(this).buildDockerImage(imageName)
+    def docker = new Docker(this)
+    return docker.buildDockerImage(imageName)
     // echo "building the docker image $imageName"
     // sh "node -v && npm i && docker -v && docker images && docker ps -a"
     // sh "docker build -t $imageName ."
