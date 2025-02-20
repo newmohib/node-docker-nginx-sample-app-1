c594cfcc3cf9456175cc18fe17b1744a55cb2ac8
newmohib <@@mohib100@@>
2025-02-20 15:16:17 +0600

    added groovy package as sorce

---
+++ com.example/Docker.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/env groovy
+
+package com.example
+
+class Docker implements Serializable {
+    def script
+    Docker(script) {
+        echo 'Initializing Docker class...'
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        echo "Building Docker image image $imageName"
+        script.sh "node -v && npm i && docker -v && docker images && docker ps -a"
+        script.sh "docker build -t $imageName ."
+    }
+
+    def pushImageToDockerHub(String imageName) {
+        echo "Pushing the docker image to docker hub... as imageName: $imageName"
+        withCredentials([usernamePassword(credentialsId:'docker-hub-personal-credential',passwordVariable:'PASS', usernameVariable:'USER')])
+        {
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+            script.sh "docker push $imageName"
+        }
+    }
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,10 @@
 #!/usr/bin/env groovy
 
+package com.example.Docker
+
 def call(String imageName){
-    echo "building the docker image $imageName"
-    sh "node -v && npm i && docker -v && docker images && docker ps -a"
-    sh "docker build -t $imageName ."
+    return new Docker(this).buildDockerImage(imageName)
+    // echo "building the docker image $imageName"
+    // sh "node -v && npm i && docker -v && docker images && docker ps -a"
+    // sh "docker build -t $imageName ."
 }
