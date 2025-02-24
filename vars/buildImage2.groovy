d3c9284c65119cfb8ddb5f233dcd418f7985d519
newmohib <@@mohib100@@>
2025-02-24 16:01:20 +0600

    rename groovy file name

--- vars/buildImage2.groovy
+++ vars/buildImage2.groovy
@@ -2,7 +2,7 @@
 
 def call(String imageName){
     def docker = new Docker(this)
-    return docker.buildDockerImages(imageName)
+    return docker.buildDockerImage2(imageName)
     // echo "building the docker image $imageName"
     // sh "node -v && npm i && docker -v && docker images && docker ps -a"
     // sh "docker build -t $imageName ."
