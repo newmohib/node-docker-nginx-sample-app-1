e31fa7a4926d87e713a5b26dacdc582739b06f44
newmohib <@@mohib100@@>
2025-02-24 16:23:04 +0600

    updated buildImageForPush

--- vars/buildImageForPush.groovy
+++ vars/buildImageForPush.groovy
@@ -1,9 +1,7 @@
 #!/usr/bin/env groovy
 
+
 def call(String imageName){
     def docker = new Docker(this)
-    return docker.buildDockerImage(imageName)
-    // echo "building the docker image $imageName"
-    // sh "node -v && npm i && docker -v && docker images && docker ps -a"
-    // sh "docker build -t $imageName ."
+    return docker.pushImageToDockerHub(imageName)
 }
