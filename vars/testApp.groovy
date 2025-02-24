3dcb1455c8b022824032d376da6b272c9f38ac05
newmohib <@@mohib100@@>
2025-02-24 16:56:07 +0600

    test update

--- vars/testApp.groovy
+++ vars/testApp.groovy
@@ -1,5 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(){
-     echo 'Testing the application...'
+// def call(){
+//      echo 'Testing the application...'
+// }
+
+def call(String imageName){
+    def docker = new Docker(this)
+    return docker.buildDockerImagePush(imageName)
 }
