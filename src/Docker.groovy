76df8445e4cc32a1408eff7291d297d14c3c1ed0
newmohib <@@mohib100@@>
2025-02-24 16:19:10 +0600

    remove unused code

--- src/Docker.groovy
+++ src/Docker.groovy
@@ -20,17 +20,6 @@ class Docker implements Serializable {
         }
     }
 
-    def buildDockerImage2(String imageName) {
-        script.echo "Building Docker image $imageName"
-        script.sh "node -v && npm i && docker -v && docker images && docker ps -a"
-        script.sh "docker build -t $imageName ."
-        script.echo "Logging in to docker hub... for image $imageName"
-        withCredentials([usernamePassword(credentialsId:'docker-hub-personal-credential',passwordVariable:'PASS', usernameVariable:'USER')])
-        {
-            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
-        }
-    }
-
 
     def pushImageToDockerHub(String imageName) {
         script.echo "Pushing the docker image to docker hub... as imageName: $imageName"
