7835b01307c47748406c3859985f97a59ff74198
newmohib <@@mohib100@@>
2025-03-04 09:26:13 +0600

    update application port

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const port = 3000;
+const port = 4000;
 
-const appName = process.env.APP_NAME
+const appName = process.env.APP_NAME;
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-    console.log(`Request served by ${appName}`);
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+  console.log(`Request served by ${appName}`);
 });
 
 app.listen(port, () => {
-    console.log(`${appName} is listening on port ${port}`);
+  console.log(`${appName} is listening on port ${port}`);
 });
